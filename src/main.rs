5154ab30b28ed0d1a97c366cf67d70146a06e70b
Maxwell Heeschen <gorpmaster@gmail.com>
2021-04-21 22:02:24 -0500

    Remove unnecessary let mut

--- src/main.rs
+++ src/main.rs
@@ -6,24 +6,24 @@ mod grid;
 mod node;
 
 fn main() {
-    let maze = binary_tree(Grid::new(7, 5));
+    let mut graph = Grid::new(30, 11);
+    let maze = binary_tree(&mut graph);
     maze.print_ascii();
 }
 
-fn binary_tree(input_graph: Grid) -> Grid {
-    let mut graph = input_graph;
-    for i in graph.graph.node_indices() {
+fn binary_tree(input_graph: &mut Grid) -> &mut Grid {
+    for i in input_graph.graph.node_indices() {
         let mut neighbors = Vec::<NodeIndex>::new();
-        if let Some(north) = graph.graph[i].north {
+        if let Some(north) = input_graph.graph[i].north {
             neighbors.push(north);
         }
-        if let Some(east) = graph.graph[i].east {
+        if let Some(east) = input_graph.graph[i].east {
             neighbors.push(east);
         }
 
         if let Some(neighbor) = neighbors.choose(&mut rand::thread_rng()) {
-            graph.graph.add_edge(i, *neighbor, 1);
+            input_graph.graph.add_edge(i, *neighbor, 1);
         }
     }
-    graph
+    input_graph
 }
