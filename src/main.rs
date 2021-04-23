222b85819eb4d31020dae2562a96f3d1a03406ca
Maxwell Heeschen <gorpmaster@gmail.com>
2021-04-22 22:22:26 -0500

    Implement sidewinder algorithm

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,9 @@ mod grid;
 mod node;
 
 fn main() {
-    let mut graph = Grid::new(30, 11);
-    let maze = binary_tree(&mut graph);
+    let mut graph = Grid::new(20, 11);
+    //let maze = binary_tree(&mut graph);
+    let maze = sidewinder(&mut graph);
     maze.print_ascii();
 }
 
@@ -23,6 +24,34 @@ fn binary_tree(input_graph: &mut Grid) -> &mut Grid {
 
         if let Some(neighbor) = neighbors.choose(&mut rand::thread_rng()) {
             input_graph.graph.add_edge(i, *neighbor, 1);
+        }
+    }
+    input_graph
+}
+
+fn sidewinder(input_graph: &mut Grid) -> &mut Grid {
+    for row in input_graph.each_row() {
+        let mut run = Vec::<NodeIndex>::new();
+        for cell in row {
+            run.push(cell);
+            let at_eastern_boundary = input_graph.graph[cell].east.is_none();
+            let at_northern_boundary = input_graph.graph[cell].north.is_none();
+
+            let should_close =
+                at_eastern_boundary || (!at_northern_boundary && rand::random::<bool>());
+
+            if should_close {
+                if let Some(cut) = run.choose(&mut rand::thread_rng()) {
+                    if let Some(north_cut) = input_graph.graph[*cut].north {
+                        input_graph.graph.add_edge(*cut, north_cut, 1);
+                    }
+                }
+                run.clear();
+            } else {
+                input_graph
+                    .graph
+                    .add_edge(cell, input_graph.graph[cell].east.unwrap(), 1);
+            }
         }
     }
     input_graph
