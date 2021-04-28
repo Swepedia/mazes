f671cc72fac81e3c1bce0bb2892250052a34d6a0
Maxwell Heeschen <gorpmaster@gmail.com>
2021-04-27 20:51:08 -0500

    Implement Aldous-Broder algorithm

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,11 @@ mod grid;
 mod node;
 
 fn main() {
-    let mut graph = Grid::new(70, 70);
-    let maze = binary_tree(&mut graph);
-    //let maze = sidewinder(&mut graph);
-    //maze.print_ascii();
-    maze.print_png(15);
+    let mut graph = Grid::new(40, 40);
+    //let maze = binary_tree(&mut graph);
+    let maze = aldous_broder(&mut graph);
+    maze.print_ascii();
+    //maze.print_png(15);
 }
 
 fn binary_tree(input_graph: &mut Grid) -> &mut Grid {
@@ -55,5 +55,36 @@ fn sidewinder(input_graph: &mut Grid) -> &mut Grid {
             }
         }
     }
+    input_graph
+}
+
+fn aldous_broder(input_graph: &mut Grid) -> &mut Grid {
+    let all_cells: Vec<NodeIndex> = input_graph.graph.node_indices().collect();
+    let mut current_cell = *all_cells.choose(&mut rand::thread_rng()).unwrap();
+    let mut unvisited = all_cells.len() - 1;
+
+    while unvisited > 0 {
+        let mut neighbors = Vec::<NodeIndex>::new();
+        if let Some(north) = input_graph.graph[current_cell].north {
+            neighbors.push(north);
+        }
+        if let Some(east) = input_graph.graph[current_cell].east {
+            neighbors.push(east);
+        }
+        if let Some(south) = input_graph.graph[current_cell].south {
+            neighbors.push(south);
+        }
+        if let Some(west) = input_graph.graph[current_cell].west {
+            neighbors.push(west);
+        }
+
+        let neighbor = *neighbors.choose(&mut rand::thread_rng()).unwrap();
+        if input_graph.graph.neighbors(neighbor).count() == 0 {
+            input_graph.graph.add_edge(current_cell, neighbor, 1);
+            unvisited -= 1;
+        }
+        current_cell = neighbor;
+    }
+
     input_graph
 }
